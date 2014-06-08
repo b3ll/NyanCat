9aa463590b3d6612b4a984f8a78482761087d2f3
Adam Bell <b3ll@fb.com>
2014-06-08 10:58:47 -0700

    yolo

---
+++ Today/TodayViewController.h
@@ -0,0 +1,15 @@
+//
+//  TodayViewController.h
+//  Today
+//
+//  Created by Adam Bell on 6/7/14.
+//  Copyright (c) 2014 Adam Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@property IBOutlet UIImageView *nyanCatImageview;
+
+@end
---
+++ Today/TodayViewController.m
@@ -0,0 +1,51 @@
+//
+//  TodayViewController.m
+//  Today
+//
+//  Created by Adam Bell on 6/7/14.
+//  Copyright (c) 2014 Adam Bell. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self != nil) {
+    }
+
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.nyanCatImageview.image = [UIImage imageNamed:@"Portrait-Nyan-1.png"];
+    self.nyanCatImageview.clipsToBounds = NO;
+    
+    NSMutableArray *array = [NSMutableArray array];
+    for (int i = 1; i < 13; i++) {
+        [array addObject:[UIImage imageNamed:[NSString stringWithFormat:@"Portrait-Nyan-%d.png", i]]];
+    }
+    self.nyanCatImageview.animationImages = array;
+    self.nyanCatImageview.animationDuration = 1.0;
+    self.nyanCatImageview.animationRepeatCount = INT_MAX;
+    [self.nyanCatImageview startAnimating];
+    
+    self.view.clipsToBounds = NO;
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler
+{
+    completionHandler(NCUpdateResultNewData);
+}
+
+@end
